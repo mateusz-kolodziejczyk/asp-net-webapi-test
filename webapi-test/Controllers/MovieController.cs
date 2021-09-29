3cb382aeb01f0996ecf14f652e386e829b003668
Mateusz Kolodziejczyk <20084190@mail.wit.ie>
2021-09-29 14:31:00 +0100

    Initial commit

---
+++ webapi-test/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public Movie Get()
+        {
+            return new Movie{Title = "x", LeadActor = "y"};
+        }
+    }
+}
---
+++ webapi-test/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace webapi_test
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string LeadActor { get; set; }
+    }
+}
